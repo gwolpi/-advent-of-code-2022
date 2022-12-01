ebf561ec4dbf2457af7f2b63460567ba557b2e05
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-01 13:46:50 +0100

    🧝 Day 01

--- days/01.ts
+++ days/01.ts
@@ -1,11 +1,11 @@
 const processInput = (input: string): number[] => {
-  return input.split('\n\n')
-      .map((elf) => elf.split('\n').reduce((curr, next) => curr + +next, 0));
+	return input.split('\n\n')
+		.map((elf) => elf.split('\n').reduce((curr, next) => curr + +next, 0));
 };
 
 export const p1 = (input: string): number => Math.max(...processInput(input));
 
 export const p2 = (input: string): number => {
-  const [x, y, z] = processInput(input).sort((a, b) => b - a);
-  return x + y + z;
+	const [x, y, z] = processInput(input).sort((a, b) => b - a);
+	return x + y + z;
 }
--- index.ts
+++ index.ts
@@ -1,43 +1,43 @@
-import { ensureDir } from "https://deno.land/std@0.166.0/fs/ensure_dir.ts";
-import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-import { brightYellow as yellow, underline, brightBlue as blue } from "https://deno.land/std@0.116.0/fmt/colors.ts";
+import {ensureDir} from "https://deno.land/std@0.166.0/fs/ensure_dir.ts";
+import {config} from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import {brightBlue as blue, brightYellow as yellow, underline as ul} from "https://deno.land/std@0.116.0/fmt/colors.ts";
 
-const [ cmd, dayArg, partNumber ] = Deno.args;
+const [cmd, dayArg, partArg] = Deno.args;
 const dayNumber = Number(dayArg);
 if (isNaN(dayNumber)) throw new Error('Day number provided is incorrect')
-const day = `${dayNumber}`.padStart(2, '0');
-const env = config();
+const dayCode = `${dayNumber}`.padStart(2, '0');
+const {ADVENT_YEAR, ADVENT_SESSION_TOKEN} = config();
 
-switch(cmd) {
-  case 'run': {
-    const file = await import(`./days/${day}.ts`);
-    const response = await fetch(`https://adventofcode.com/${env.ADVENT_YEAR}/day/${dayNumber}/input`, { headers: { cookie: `session=${env.ADVENT_SESSION_TOKEN}` }});
-    if (!response.ok) throw new Error('Advent of Code Session Token not set');
-    const input = await response.text();
-    const runningDay = `${blue('Running day')} ${yellow(day)} ${blue('part')}`;
-    const runPart = (x?: number) => {
-      if (!(!partNumber || x === +partNumber)) return;
-      const timerStart = performance.now();
-      const result = file[`p${x}`](input);
-      const timerEnd = performance.now() - timerStart;
-      console.log(underline(`${runningDay} ${yellow(`${x}`)}`));
-      console.log(`${yellow('[Answer]\t')} ${result}`);
-      console.log(`${yellow('[Time]\t\t')} ~${timerEnd.toFixed(3)}ms\n`);
-    }
-    runPart(1);
-    runPart(2);
-    break;
-  }
-  case 'create': {
-    console.log(`Creating day ${dayNumber}`)
-    const template = await Deno.readTextFile('./template.ts.txt');
-    const testTemplate = await Deno.readTextFile('./template.test.ts.txt');
-    await ensureDir(`./days`);
-    await Deno.writeTextFile(`./days/${day}.ts`, template);
-    await Deno.writeTextFile(`./days/${day}.test.ts`, testTemplate.replaceAll('{DAYNUMBER}', day));
-    break;
-  }
-  default: {
-    throw new Error('Command not found');
-  }
+switch (cmd) {
+	case 'run': {
+		const file = await import(`./days/${dayCode}.ts`);
+		const response = await fetch(`https://adventofcode.com/${ADVENT_YEAR}/day/${dayNumber}/input`, {headers: {cookie: `session=${ADVENT_SESSION_TOKEN}`}});
+		if (!response.ok) throw new Error('Error while fetching input, maybe your session token is expired?');
+		const input = await response.text();
+		const runningMsg = `Running day ${dayNumber} part`;
+		const runPart = (partNumber?: number) => {
+			if (!(!partArg || partNumber === +partArg)) return;
+			const timerStart = performance.now();
+			const result = file[`p${partNumber}`](input);
+			const timerEnd = performance.now() - timerStart;
+			console.log(ul(blue(`${runningMsg} ${partNumber}.`)));
+			console.log(`${yellow('[Answer]\t')} ${result}`);
+			console.log(`${yellow('[Time]\t\t')} ~${timerEnd.toFixed(3)}ms\n`);
+		}
+		runPart(1);
+		runPart(2);
+		break;
+	}
+	case 'create': {
+		console.log(`Creating day ${dayNumber}`);
+		const template = await Deno.readTextFile('./template.ts.txt');
+		const testTemplate = await Deno.readTextFile('./template.test.ts.txt');
+		await ensureDir(`./days`);
+		await Deno.writeTextFile(`./days/${dayCode}.ts`, template);
+		await Deno.writeTextFile(`./days/${dayCode}.test.ts`, testTemplate.replaceAll('{DAYNUMBER}', dayCode));
+		break;
+	}
+	default: {
+		throw new Error('Command not found');
+	}
 }
