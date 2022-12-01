822640a13d9841711b8627adb637b66ce9f9282e
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-01 18:37:00 +0100

    🧝 Day 01

--- days/01.ts
+++ days/01.ts
@@ -1,3 +1,5 @@
+import '../extension-methods.ts';
+
 const processInput = (input: string): number[] => input.splitRows(2).map((elf) => elf.splitRows().sum());
 export const p1 = (input: string): number => processInput(input).max();
 export const p2 = (input: string): number => processInput(input).sortNums().take(-3).sum()
--- extension-methods.ts
+++ extension-methods.ts
@@ -1,19 +1,42 @@
 declare global {
 	export interface String {
+		/**
+		 * Splits the string into an array of strings based on the amount of newline-characters.
+		 * @param length {number} The amount of newline characters between each row.
+		 */
 		splitRows(length?: number): string[];
 	}
 
 	export interface Array<T> {
+		/**
+		 * Sums all the numbers in the array
+		 */
 		sum(): number;
 
+		/**
+		 * Returns the highest number in the array
+		 */
 		max(): number;
 
+		/**
+		 * Returns the lowest number in the array
+		 */
 		min(): number;
 
+		/**
+		 * Returns the average of all the numbers in the array
+		 */
 		avg(): number;
 
+		/**
+		 * Sorts the array numerically
+		 */
 		sortNums(): number[];
 
+		/**
+		 * Returns the first n elements of the array
+		 * @param amount {number} The amount of elements to take
+		 */
 		take(amount: number): Array<T>;
 	}
 }
