baf294226e24ad07dd1483a1f513d9d6d2973bd8
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-02 17:32:01 +0100

    🫱 Day 02

--- days/02.ts
+++ days/02.ts
@@ -3,27 +3,29 @@ import '../extension-methods.ts';
 type Result = { [key: string]: number };
 type ResultSet = { [key: string]: Result };
 
+const R = 1, P = 2, S = 3;
+const W = 6, L = 0, D = 3;
+
 export const p1 = (input: string): number => input
 	.splitRows()
 	.reduce((curr, [p1, , p2]) => {
-		const choice: Result = {X: 1, Y: 2, Z: 3};
+		const choice: Result = {X: R, Y: P, Z: S};
 		const outcomes: ResultSet = {
-			A: {X: 3, Y: 6, Z: 0},
-			B: {X: 0, Y: 3, Z: 6},
-			C: {X: 6, Y: 0, Z: 3}
+			A: {X: D, Y: W, Z: L},
+			B: {X: L, Y: D, Z: W},
+			C: {X: W, Y: L, Z: D}
 		};
 		return curr + choice[p2] + outcomes[p1][p2];
 	}, 0);
 
-
 export const p2 = (input: string): number => input
 	.splitRows()
 	.reduce((curr, [p1, , p2]) => {
-		const outcome: Result = {X: 0, Y: 3, Z: 6};
+		const outcome: Result = {X: L, Y: D, Z: W};
 		const choices: ResultSet = {
-			A: {X: 3, Y: 1, Z: 2},
-			B: {X: 1, Y: 2, Z: 3},
-			C: {X: 2, Y: 3, Z: 1}
+			A: {X: S, Y: R, Z: P},
+			B: {X: R, Y: P, Z: S},
+			C: {X: P, Y: S, Z: R}
 		};
 		return curr + choices[p1][p2] + outcome[p2];
 	}, 0);
