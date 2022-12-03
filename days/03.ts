160fb4ef66f3ab00cf1d889c0511bd811d10f8b4
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-03 17:22:20 +0100

    🎒 Day 3

--- days/03.ts
+++ days/03.ts
@@ -5,10 +5,7 @@ export const p1 = (input: string): number => input.splitRows()
 		const c1 = rucksack.slice(0, rucksack.length / 2);
 		const c2 = rucksack.slice(rucksack.length / 2);
 		const letter = [...c1].find((letter: string) => c2.includes(letter));
-		const letterNumber = letter?.charCodeAt(0) ?? 0;
-		const padding = letterNumber >= 65 && letterNumber <= 90 ? 64 - 26 : 96;
-		const letterValue = letterNumber - padding;
-		return acc + letterValue;
+		return acc + getBadgeNumber(letter ?? '');
 	}, 0);
 
 export const p2 = (input: string): number => input.splitRows()
@@ -18,8 +15,12 @@ export const p2 = (input: string): number => input.splitRows()
 		return acc;
 	}, [] as string[][])
 	.map((elfGroup: string[]) => [...elfGroup[0]].find(x => elfGroup.every(elf => elf.includes(x))))
-	.reduce((acc, badge) => {
-		const tokenNumber = badge?.charCodeAt(0) ?? 0;
-		const padding = tokenNumber >= 65 && tokenNumber <= 90 ? 64 - 26 : 96;
-		return acc + tokenNumber - padding;
-	}, 0);
+	.reduce((acc, badge) => acc + getBadgeNumber(badge ?? ''), 0);
+
+const getBadgeNumber = (letter: string): number => {
+	const a = 'a'.charCodeAt(0) - 1, z = 'z'.charCodeAt(0);
+	const A = 'A'.charCodeAt(0) - 1, Z = 'Z'.charCodeAt(0);
+	const tokenNumber = letter?.charCodeAt(0) ?? 0;
+	const padding = tokenNumber >= A && tokenNumber <= Z ? A - (z - a) : a;
+	return tokenNumber - padding;
+}
