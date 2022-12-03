04d98decc3230ddc0e04dd48d003b03c7864985b
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-03 17:45:49 +0100

    🎒 Day 3

--- days/03.ts
+++ days/03.ts
@@ -21,6 +21,5 @@ const getBadgeNumber = (letter: string): number => {
 	const a = 'a'.charCodeAt(0) - 1, z = 'z'.charCodeAt(0);
 	const A = 'A'.charCodeAt(0) - 1, Z = 'Z'.charCodeAt(0);
 	const tokenNumber = letter?.charCodeAt(0) ?? 0;
-	const padding = tokenNumber > A && tokenNumber <= Z ? A - (z - a) : a;
-	return tokenNumber - padding;
+	return tokenNumber - tokenNumber > A && tokenNumber <= Z ? A - (z - a) : a;
 }
