b95ec4dfb395b3e75caec319e78556697b9a9311
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-04 17:26:16 +0100

    🧼 Day 4

--- days/04.ts
+++ days/04.ts
@@ -1,13 +1,13 @@
 import '../extension-methods.ts';
 
-const processLines = (input: string): [number[], number[]][] =>
+const processLines = (input: string): [number, number, number, number][] =>
 	input.matchMap(/(\d+)-(\d+),(\d+)-(\d+)/g, ([, min1, max1, min2, max2]) =>
-		[Array.range(+min1, +max1), Array.range(+min2, +max2)]);
+		[+min1, +max1, +min2, +max2]);
 
 export const p1 = (input: string): number =>
-	processLines(input).count(([pair1, pair2]) =>
-		pair1.every(n => pair2.includes(n)) || pair2.every(n => pair1.includes(n)));
+	processLines(input).reduce((acc, [min1, max1, min2, max2]) =>
+		acc + +((min1 <= min2 && max1 >= max2) || (min2 <= min1 && max2 >= max1)), 0);
 
 export const p2 = (input: string): number =>
-	processLines(input).count(([pair1, pair2]) =>
-		pair1.some(n => pair2.includes(n)) || pair2.some(n => pair1.includes(n)));
+	processLines(input).reduce((acc, [min1, max1, min2, max2]) =>
+		acc + +((min1 >= max2 && min2 >= max1) || (max1 >= min2 && max2 >= min1)), 0);
