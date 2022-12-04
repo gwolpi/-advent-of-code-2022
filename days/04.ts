a5f17280256923dc4ec19b5d2875659fb06fafe8
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-04 18:14:50 +0100

    🧼 Day 4

--- days/04.ts
+++ days/04.ts
@@ -4,9 +4,9 @@ const processLines = (input: string): number[][] =>
 	input.matchMap(/(\d+)-(\d+),(\d+)-(\d+)/g, (x) => x.slice(1, 5).map(Number));
 
 export const p1 = (input: string): number =>
-	processLines(input).reduce((acc, [min1, max1, min2, max2]) =>
-		acc + +((min1 >= min2 && max2 >= max1) || (max1 >= max2 && min2 >= min1)), 0);
+	processLines(input).count(([min1, max1, min2, max2]) =>
+		(min1 >= min2 && max2 >= max1) || (max1 >= max2 && min2 >= min1));
 
 export const p2 = (input: string): number =>
-	processLines(input).reduce((acc, [min1, max1, min2, max2]) =>
-		acc + +((min1 >= max2 && min2 >= max1) || (max1 >= min2 && max2 >= min1)), 0);
+	processLines(input).count(([min1, max1, min2, max2]) =>
+		(min1 >= max2 && min2 >= max1) || (max1 >= min2 && max2 >= min1));
