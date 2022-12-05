589ed52ba3ece926421f60f3768699740d3712c9
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-05 15:39:40 +0100

    🏗️ Day 5

--- days/05.ts
+++ days/05.ts
@@ -1,28 +1,30 @@
 import '../extension-methods.ts';
 
+
 const processInput = (input: string, reverse: boolean): string =>{
-  const [a, b] = input.split('\n\n');
-  const stacks = a
-      .replace(/^ {4}\[|] \[|]? {3,}?\[?|] {4}$/gm, ',')
-      .replace(/\[|]|[ \d]+/gm, '')
-      .split('\n')
-      .reduce((acc, curr) => {
-        curr.split(',').forEach((x, index) => {
-          if (!x) return;
-          (acc[index + 1] ||= []).unshift(x);
-          return acc;
-        });
-        return acc;
-      }, {} as { [key: string]: string[] });
-  const moves = b.matchMap(/move (\d+) from (\d+) to (\d+)/g, (x) => x.slice(1, 4).map(Number));
-  moves.forEach(([amount, from, to]) =>  {
-    const x = stacks[from].splice(-amount)
-    if (reverse) x.reverse();
-    stacks[to].push(...x)
-  });
-  return Object.values(stacks).reduce((acc, curr) => acc + curr.at(-1), '');
+    const [a, b] = input.split('\n\n');
+    const stacks = a
+        .replace(/] \[|]? {2,3}\[?/gm, ',')
+        .replace(/\[|]|[ \d]+/gm, '')
+        .split('\n')
+        .reduce((acc, curr) => {
+            curr.split(',').forEach((x, index) => {
+                if (!x) return;
+                (acc[index + 1] ||= []).unshift(x);
+                return acc;
+            });
+            return acc;
+        }, {} as { [key: string]: string[] });
+    const moves = b.matchMap(/move (\d+) from (\d+) to (\d+)/g, (x) => x.slice(1, 4).map(Number));
+    moves.forEach(([amount, from, to]) =>  {
+        const x = stacks[from].splice(-amount)
+        if (reverse) x.reverse();
+        stacks[to].push(...x)
+    });
+    return Object.values(stacks).reduce((acc, curr) => acc + curr.at(-1), '');
 }
 
 export const p1 = (input: string): string => processInput(input, true);
 
 export const p2 = (input: string): string => processInput(input, false);
+
