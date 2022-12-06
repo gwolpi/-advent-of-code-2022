581fa0bd2d03c95d245a8f72aee38dbb428fd4be
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-06 11:17:35 +0100

    📡 Day 6

--- days/06.ts
+++ days/06.ts
@@ -2,7 +2,7 @@ import '../extension-methods.ts';
 
 const processInput = (input: string, charMarker: number): number => {
 	let regex = `(.)(?!.?\\${charMarker - 2})(.)(?!\\${charMarker - 1})`;
-	for (let i = 1; i < charMarker - 2; i++) regex = `(.)(?!.{0,${i + 1}}\\${charMarker - i - 2})` + regex;
+	for (let i = 1; i < charMarker - 2; i++) regex = `(.)(?!.{0,${i + 1}}\\${charMarker - i - 2})${regex}`;
 	return (input.match(new RegExp(regex))?.index ?? 0) + charMarker;
 }
 export const p1 = (input: string): number => processInput(input, 4);
