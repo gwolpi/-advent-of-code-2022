28f331f5733b8b3d26ef1ef2cb231b1f9f9b0f4d
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-09 17:55:18 +0100

    🪢 Day 9

--- days/09.ts
+++ days/09.ts
@@ -5,11 +5,12 @@ const processInput = (input: string) => {
 	const head = {x: 0, y: 0};
 	type DirFunctions = { [key: string]: () => void };
 	const dirs: DirFunctions = {U: () => head.y--, D: () => head.y++, L: () => head.x--, R: () => head.x++};
-	return {lines, head, dirs};
+	const visited = new Set<string>('x0y0');
+	return {lines, head, dirs, visited};
 };
 
 export const p1 = (input: string): number => {
-	const {lines, head, dirs} = processInput(input);
+	const {lines, head, dirs, visited} = processInput(input);
 	const tail = {x: 0, y: 0};
 	return lines.reduce((visited, {dir, steps}) => {
 		for (let step = 0; step < steps; step++) {
@@ -26,11 +27,11 @@ export const p1 = (input: string): number => {
 			visited.add(`x${tailX}y${tailY}`);
 		}
 		return visited
-	}, new Set(["x0y0"])).size;
+	}, visited).size;
 }
 
 export const p2 = (input: string): number => {
-	const {lines, head, dirs} = processInput(input);
+	const {lines, head, dirs, visited} = processInput(input);
 	const tails = Array.from({length: 9}, () => ({x: 0, y: 0}));
 	return lines.reduce((visited, {dir, steps}) => {
 		for (let step = 0; step < steps; step++) {
@@ -54,5 +55,5 @@ export const p2 = (input: string): number => {
 			visited.add(`x${tailX}y${tailY}`);
 		}
 		return visited
-	}, new Set(["x0y0"])).size;
+	}, visited).size;
 }
