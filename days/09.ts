247efed7eaf309575ba80ca495644c1cedf809a6
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-09 19:31:19 +0100

    🪢 Day 9

--- days/09.ts
+++ days/09.ts
@@ -1,50 +1,48 @@
 import '../extension-methods.ts';
 
-const processInput = (input: string) => {
-	const lines = input.matchMap(/([A-Z])\s(\d+)/g, ([, dir, steps]) => ({dir, steps: +steps}));
-	const head = {x: 0, y: 0};
-	type DirFunctions = { [key: string]: () => void };
+type MotionMove = { dir: string, steps: number };
+type Coord = { x: number, y: number };
+type DirFunctions = { [key: string]: () => void };
+type ProcessedInput = { head: Coord, dirs: DirFunctions, motionMoves?: MotionMove[], visited?: Set<string> };
+
+const processInput = (input: string): ProcessedInput => {
+	const head: Coord = {x: 0, y: 0};
 	const dirs: DirFunctions = {U: () => head.y--, D: () => head.y++, L: () => head.x--, R: () => head.x++};
+	const lines = input.matchMap(/([A-Z])\s(\d+)/g, ([, dir, steps]) => ({dir, steps: +steps}));
 	const visited = new Set(['x0y0']);
-	return {lines, head, dirs, visited};
+	return {head, dirs, motionMoves: lines, visited};
 };
 
+const processStep = ({dir, steps}: MotionMove, {dirs, head}: ProcessedInput, tails: Coord[]): string => {
+	dirs[dir]();
+	tails.forEach((tail, index) => {
+		const prevTail = tails[index - 1] ?? head;
+		const xDiff = prevTail.x - tail.x, yDiff = prevTail.y - tail.y;
+		const xAbs = Math.abs(xDiff) > 1, yAbs = Math.abs(yDiff) > 1;
+		if (!(xAbs || yAbs)) return;
+		tail.x = xAbs ? tail.x + Math.sign(xDiff) : prevTail.x
+		tail.y = yAbs ? tail.y + Math.sign(yDiff) : prevTail.y
+	});
+	const {x, y} = tails.at(-1)!;
+	return `x${x}y${y}`
+}
+
 export const p1 = (input: string): number => {
-	const {lines, head, dirs, visited} = processInput(input);
+	const {head, dirs, motionMoves, visited} = processInput(input);
 	const tail = {x: 0, y: 0};
-	return lines.reduce((acc, {dir, steps}) => {
-		for (let step = 0; step < steps; step++) {
-			dirs[dir]();
-			const xDiff = head.x - tail.x, yDiff = head.y - tail.y;
-			const xAbs = Math.abs(xDiff) > 1, yAbs = Math.abs(yDiff) > 1;
-			if (xAbs || yAbs) {
-				tail.x = xAbs ? tail.x + Math.sign(xDiff) : head.x
-				tail.y = yAbs ? tail.y + Math.sign(yDiff) : head.y
-			}
-			const {x, y} = tail;
-			acc.add(`x${x}y${y}`);
-		}
+	return motionMoves.reduce((acc, line) => {
+		for (let step = 0; step < line.steps; step++)
+			acc.add(processStep(line, {dirs, head}, [tail]));
 		return acc
 	}, visited).size;
 }
 
 export const p2 = (input: string): number => {
-	const {lines, head, dirs, visited} = processInput(input);
+	const {head, dirs, motionMoves, visited} = processInput(input);
 	const tails = Array.from({length: 9}, () => ({x: 0, y: 0}));
-	return lines.reduce((acc, {dir, steps}) => {
-		for (let step = 0; step < steps; step++) {
-			dirs[dir]();
-			tails.forEach((tail, index) => {
-				const prevTail = tails[index - 1] ?? head;
-				const xDiff = prevTail.x - tail.x, yDiff = prevTail.y - tail.y;
-				const xAbs = Math.abs(xDiff) > 1, yAbs = Math.abs(yDiff) > 1;
-				if (!(xAbs || yAbs)) return;
-				tail.x = xAbs ? tail.x + Math.sign(xDiff) : prevTail.x
-				tail.y = yAbs ? tail.y + Math.sign(yDiff) : prevTail.y
-			});
-			const {x, y} = tails.at(-1)!;
-			acc.add(`x${x}y${y}`);
-		}
+	return motionMoves.reduce((acc, line) => {
+		for (let step = 0; step < line.steps; step++)
+			acc.add(processStep(line, {dirs, head}, tails));
 		return acc
 	}, visited).size;
 }
