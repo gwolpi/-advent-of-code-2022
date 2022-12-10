993a4513bf1de98792fd19567b02e363cd05287c
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-10 16:21:50 +0100

    🖥️ Day 10

--- days/10.ts
+++ days/10.ts
@@ -1,16 +1,18 @@
 import '../extension-methods.ts';
 
 const processInput = (input: string) => `noop\n${input}`
+  // Add a noop operation before each addx operation to simulate two cycles
   .replace(/(addx -?\d+)/g, 'noop\n$1')
-  .matchMap(/^(\w+)(?: (-?\d+))?$/gm,
-    ([, operationName, value]) => ({ changeRegister: operationName === 'addx', value: +value }));
+  .matchMap(/^\w+(?: (-?\d+))?$/gm, ([, value]) => +value);
 
 export const p1 = (input: string): number => {
   let register = 1;
   const cycles = processInput(input);
-  return cycles.reduce((acc, { changeRegister, value }, index) => {
+  return cycles.reduce((acc, value, index) => {
     const cycleNumber = index + 1;
-    if (changeRegister) register += value;
+    // if value is a valid number, it is an addx operation and should be added to the register
+    if (!isNaN(value)) register += value;
+    // Add the signal strength to the accumulator if the cycle number is 20, 60, 100, 140, 180 or 220
     if (cycleNumber % 40 === 20) acc += register * cycleNumber;
     return acc;
   }, 0);
@@ -19,10 +21,13 @@ export const p1 = (input: string): number => {
 export const p2 = (input: string): string => {
   let register = 1;
   const cycles = processInput(input);
-  return cycles.reduce((acc, { changeRegister, value }, cycleNumber) => {
-    if (changeRegister) register += value;
+  return cycles.reduce((acc, value, cycleNumber) => {
+    // if value is a valid number, it is an addx operation and should be added to the register
+    if (!isNaN(value)) register += value;
     const pos = cycleNumber % 40;
+    // Position reached the end of a row, add a newline character
     if (!pos) acc += '\n';
+    // When the pixel is -1, 0 or 1, lit the pixel else leave it dark
     acc += (Math.abs(register - pos) <= 1) ? '#' : ' ';
     return acc;
   }, '').trimEnd();
