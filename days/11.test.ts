b2778b7e880868822e038acf2b67d724e851c440
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-11 21:33:11 +0100

    🙈 Day 11

--- days/11.test.ts
+++ days/11.test.ts
@@ -3,23 +3,23 @@ import {p1, p2} from './11.ts';
 import {assertEquals} from "https://deno.land/std@0.166.0/testing/asserts.ts";
 
 const input = '' +
-  'Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19' +
-  '\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\n' +
-  'Monkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6' +
-  '\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\n' +
-  'Monkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old' +
-  '\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\n' +
-  'Monkey 3:\n  Starting items: 74\n  Operation: new = old + 3' +
-  '\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1';
+	'Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19' +
+	'\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\n' +
+	'Monkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6' +
+	'\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\n' +
+	'Monkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old' +
+	'\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\n' +
+	'Monkey 3:\n  Starting items: 74\n  Operation: new = old + 3' +
+	'\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1';
 
 Deno.test('it should run the first part of day 11 correctly', () => {
-  const result = p1(input);
-  const expected = 10605;
-  assertEquals(result, expected);
+	const result = p1(input);
+	const expected = 10605;
+	assertEquals(result, expected);
 });
 
 Deno.test('it should run the second part of day 11 correctly', () => {
-  const result = p2(input);
-  const expected = 0;
-  assertEquals(result, expected);
+	const result = p2(input);
+	const expected = 2713310158;
+	assertEquals(result, expected);
 });
--- days/11.ts
+++ days/11.ts
@@ -1,14 +1,14 @@
 import '../extension-methods.ts';
 
-type Monkey = { id: number, items: number[], operator: string, operationVal: string | number, testDivision: number, conditionTrue: number, conditionFalse: number, acc: number };
+type Monkey = { id: number, items: number[], operator: string, operationValue: string | number, testDivision: number, conditionTrue: number, conditionFalse: number, inspectionCount: number };
 
-const processInput = (input: string): Monkey[] => {
+const processInput = (input: string): { [key: number]: Monkey } => {
 	const regex = /Monkey (\d+):\n {2}Starting items: ([\d, ]+)\n {2}Operation: new = old ([*+]) (\d+|\w+)\n {2}Test: divisible by (\d+)\n {4}If true: throw to monkey (\d+)\n {4}If false: throw to monkey (\d)/gm;
 	return input.matchMap(regex, ([, id, items, operator, operationVal, testDivision, conditionTrue, conditionFalse]) => ({
 		id: +id,
 		items: items.split(', ').map(x => +x),
 		operator,
-		operationVal: isNaN(+operationVal) ? operationVal : +operationVal,
+		operationValue: isNaN(+operationVal) ? operationVal : +operationVal,
 		testDivision: +testDivision,
 		conditionTrue: +conditionTrue,
 		conditionFalse: +conditionFalse,
@@ -20,50 +20,52 @@ const processInput = (input: string): Monkey[] => {
 }
 
 export const p1 = (input: string): number => {
-	const monkeys = processInput(input);
+	const monkeysMap = processInput(input), monkeysArray = Object.values(monkeysMap);
 	for (let i = 0; i < 20; i++) {
-		for (const monkey of Object.values(monkeys)) {
-			monkey.items.forEach((worry) => {
-				const operationValue = typeof monkey.operationVal === 'number' ? monkey.operationVal : worry
-				const operators = {
-					'*': () => worry *= operationValue,
-					'+': () => worry += operationValue,
+		for (const monkey of monkeysArray) {
+			const {items, operator, operationValue, testDivision, conditionTrue, conditionFalse} = monkey;
+			items.forEach((worry) => {
+				const value = typeof operationValue === 'number' ? operationValue : worry
+				const operators: { [key: string]: () => number } = {
+					'*': () => worry * value,
+					'+': () => worry + value,
 				}
-				worry = Math.floor(operators[monkey.operator]() / 3);
-				const targetMonkey = worry % monkey.testDivision === 0 ? monkey.conditionTrue : monkey.conditionFalse;
-				monkeys[targetMonkey].items.push(worry);
+				worry = Math.floor(operators[operator]() / 3);
+				const target = worry % testDivision === 0 ? conditionTrue : conditionFalse;
+				monkeysMap[target].items.push(worry);
 				monkey.inspectionCount++;
 			});
 			// Prevent infinite loop
-			monkey.items.length = 0;
+			items.length = 0;
 		}
 	}
 
-	const [a, b] = Object.values(monkeys).map(x => x.inspectionCount).sort((a, b) => b - a);
-	return a * b;
+	const result = monkeysArray.map(x => x.inspectionCount).sortNums();
+	return result.at(-1)! * result.at(-2)!;
 }
 
 export const p2 = (input: string): number => {
-	const monkeys = processInput(input);
-	const safeDivisible = Object.values(monkeys).map(({testDivision}) => testDivision).reduce((a, b) => a * b);
+	const monkeysMap = processInput(input), monkeysArray = Object.values(monkeysMap);
+	const safeDivisible = monkeysArray.map(({testDivision}) => testDivision).reduce((a, b) => a * b);
 	for (let i = 0; i < 10000; i++) {
-		for (const monkey of Object.values(monkeys)) {
-			monkey.items.forEach((worry) => {
-				const operationValue = typeof monkey.operationVal === 'number' ? monkey.operationVal : worry
-				const operators = {
-					'*': () => worry *= operationValue,
-					'+': () => worry += operationValue,
+		for (const monkey of monkeysArray) {
+			const {items, operator, operationValue, testDivision, conditionTrue, conditionFalse} = monkey;
+			items.forEach((worry: number) => {
+				const value = typeof operationValue === 'number' ? operationValue : worry
+				const operators: { [key: string]: () => number } = {
+					'*': () => (worry * value),
+					'+': () => (worry + value),
 				}
-				worry = operators[monkey.operator]() % safeDivisible;
-				const target = worry % monkey.testDivision === 0 ? monkey.conditionTrue : monkey.conditionFalse;
-				monkeys[target].items.push(worry);
+				worry = operators[operator]() % safeDivisible;
+				const target = worry % testDivision === 0 ? conditionTrue : conditionFalse;
+				monkeysMap[target].items.push(worry);
 				monkey.inspectionCount++;
 			});
 			// Prevent infinite loop
-			monkey.items.length = 0;
+			items.length = 0;
 		}
 	}
 
-	const [a, b] = Object.values(monkeys).map(x => x.inspectionCount).sort((a, b) => b - a);
-	return a * b;
+	const result = monkeysArray.map(({inspectionCount}) => inspectionCount).sortNums();
+	return result.at(-1)! * result.at(-2)!;
 }
