b6492d605cac2cac3bf801a573be8a040a89791a
Gwolpi <gwolpi@users.noreply.github.com>
2022-12-12 00:19:40 +0100

    🙈 Day 11

--- days/11.ts
+++ days/11.ts
@@ -1,11 +1,10 @@
 import '../extension-methods.ts';
 
-type Monkey = { id: number, items: number[], operator: string, operationValue: number, testDivision: number, conditionTrue: number, conditionFalse: number, inspectionCount: number };
+type Monkey = { items: number[], operator: string, operationValue: number, testDivision: number, conditionTrue: number, conditionFalse: number, inspectionCount: number };
 
 const processInput = (input: string): Monkey[] =>
-	input.matchMap(/^.+(\d+):\n.+:\s([\d, ]+)\n.+([*+])\s(\d+|old)\n.+y\s(\d+)\n.+y\s(\d+)\n.+y\s(\d+)$/gm,
-		([, id, items, operator, operationVal, testDivision, conditionTrue, conditionFalse]) => ({
-			id: +id,
+	input.matchMap(/^.+:\s([\d, ]+)\n.+([*+])\s(\d+|old)\n.+y\s(\d+)\n.+y\s(\d+)\n.+y\s(\d+)$/gm,
+		([, items, operator, operationVal, testDivision, conditionTrue, conditionFalse]) => ({
 			items: items.split(', ').map(x => +x),
 			operator,
 			operationValue: +operationVal,
@@ -23,19 +22,16 @@ const operators: { [key: string]: (itemNumber: number, operationValue: number) =
 export const p1 = (input: string): number => {
 	const monkeys = processInput(input);
 	for (let i = 0; i < 20; i++)
-		for (const monkey of monkeys) {
+		monkeys.forEach((monkey: Monkey) => {
 			const {items, operator, operationValue, testDivision, conditionTrue, conditionFalse} = monkey;
 			items.forEach((itemNumber: number) => {
 				const worry = Math.floor(operators[operator](itemNumber, operationValue) / 3);
 				const target = !(worry % testDivision) ? conditionTrue : conditionFalse;
-				monkeys.find(({id}) => id === target)!.items.push(worry);
+				monkeys[target].items.push(worry);
 				monkey.inspectionCount++;
 			});
-			// Prevent infinite loop
-			// Emptying an array without breaking its references by making its length = 0;
 			items.length = 0;
-		}
-
+		});
 	const result = monkeys.map(x => x.inspectionCount).sortNums();
 	return result.at(-1)! * result.at(-2)!;
 }
@@ -44,19 +40,16 @@ export const p2 = (input: string): number => {
 	const monkeys = processInput(input);
 	const safeDivision = monkeys.map(({testDivision}) => testDivision).reduce((a, b) => a * b);
 	for (let i = 0; i < 10000; i++)
-		for (const monkey of monkeys) {
+		monkeys.forEach((monkey: Monkey) => {
 			const {items, operator, operationValue, testDivision, conditionTrue, conditionFalse} = monkey;
 			items.forEach((itemNumber: number) => {
 				const worry = operators[operator](itemNumber, operationValue) % safeDivision;
 				const target = !(worry % testDivision) ? conditionTrue : conditionFalse;
-				monkeys.find(({id}) => id === target)!.items.push(worry);
+				monkeys[target].items.push(worry);
 				monkey.inspectionCount++;
 			});
-			// Prevent infinite loop
-			// Emptying an array without breaking its references by making its length = 0;
 			items.length = 0;
-		}
-
+		});
 	const result = monkeys.map(({inspectionCount}) => inspectionCount).sortNums();
 	return result.at(-1)! * result.at(-2)!;
 }
